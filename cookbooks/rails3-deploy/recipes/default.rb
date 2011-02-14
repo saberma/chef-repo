f7512edad05f54fa6f61275e3c1fceba9d1ced57
saberma <mahb45@gmail.com>
2011-02-14 09:44:36 +0800

    add staging attributes.

--- cookbooks/rails3-deploy/recipes/default.rb
+++ cookbooks/rails3-deploy/recipes/default.rb
@@ -73,7 +73,7 @@ search(:apps) do |app|
     user app['owner']
     group app['group']
     deploy_to app['deploy_to']
-    environment 'RAILS_ENV' => node.app_environment, 'MONGODB_URI' => app['mongodb_uri']
+    environment 'GEM_HOME' => node[:languages][:ruby][:gems_dir], 'RAILS_ENV' => node.app_environment, 'MONGODB_URI' => app['mongodb_uri']
     action app['force'][node.app_environment] ? :force_deploy : :deploy
 
     before_migrate do
